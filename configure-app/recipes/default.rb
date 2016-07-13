e29e1965153f34692e220a68fe8b3e744186e93a
Josh P <joshpelz@users.noreply.github.com>
2016-07-13 06:16:06 -0700

    remove caps from path (bad path)

--- configure-app/recipes/default.rb
+++ configure-app/recipes/default.rb
@@ -1,11 +1,11 @@
 # Setup environment for app
 
 execute 'pull down composer' do
-  cwd '/srv/www/AcromineDemo/current'
+  cwd '/srv/www/acrominedemo/current'
   command 'curl -sS https://getcomposer.org/installer | php'
 end
 
 execute 'configure Guzzle for app' do
-  cwd '/srv/www/AcromineDemo/current'
+  cwd '/srv/www/acrominedemo/current'
   command 'php composer.phar require guzzlehttp/guzzle'
 end
