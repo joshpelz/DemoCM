7a93e4dab55097eb34eaf4b5ee58418c8e913cc0
Josh P <joshpelz@users.noreply.github.com>
2016-07-13 07:30:32 -0700

    add prototype .js

--- configure-app/recipes/default.rb
+++ configure-app/recipes/default.rb
@@ -1,5 +1,10 @@
 # Setup environment for app
 
+execute 'download prototypel.js' do
+  cwd 'srv/www/acrominedemo/current'
+  group 'apache'
+  command 'curl -sS https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js'
+
 execute 'pull down composer' do
   cwd '/srv/www/acrominedemo/current'
   group 'apache'
