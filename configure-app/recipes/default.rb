2a888f441b367401df9e2d07322d5f01c558ebb4
Josh P <joshpelz@users.noreply.github.com>
2016-07-13 06:07:26 -0700

    fix syntax error

--- configure-app/recipes/default.rb
+++ configure-app/recipes/default.rb
@@ -1,11 +1,11 @@
 # Setup environment for app
 
-execute 'pull down composer'
+execute 'pull down composer' do
   cwd '/srv/www/AcromineDemo/current'
   command 'curl -sS https://getcomposer.org/installer | php'
 end
 
-execute 'configure Guzzle for app'
+execute 'configure Guzzle for app' do
   cwd '/srv/www/AcromineDemo/current'
   command 'php composer.phar require guzzlehttp/guzzle'
 end
