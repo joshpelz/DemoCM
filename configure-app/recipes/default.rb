632fc34a916a916b78e01f888e08b673a7cc3760
Josh P <joshpelz@users.noreply.github.com>
2016-07-13 07:22:42 -0700

    remove 'deploy' user setting

--- configure-app/recipes/default.rb
+++ configure-app/recipes/default.rb
@@ -2,14 +2,12 @@
 
 execute 'pull down composer' do
   cwd '/srv/www/acrominedemo/current'
-  user 'deploy'
   group 'apache'
   command 'curl -sS https://getcomposer.org/installer | php'
 end
 
 execute 'configure Guzzle for app' do
   cwd '/srv/www/acrominedemo/current'
-  user 'deploy'
   group 'apache'
   command 'php composer.phar require guzzlehttp/guzzle'
 end
