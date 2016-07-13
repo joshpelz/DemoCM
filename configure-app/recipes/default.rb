e2dc3dcc7421b39ebba5089ae535ecd53c519e60
Josh P <joshpelz@users.noreply.github.com>
2016-07-13 07:18:08 -0700

    Composer actions to match app user and group

--- configure-app/recipes/default.rb
+++ configure-app/recipes/default.rb
@@ -2,10 +2,14 @@
 
 execute 'pull down composer' do
   cwd '/srv/www/acrominedemo/current'
+  user 'deploy'
+  group 'apache'
   command 'curl -sS https://getcomposer.org/installer | php'
 end
 
 execute 'configure Guzzle for app' do
   cwd '/srv/www/acrominedemo/current'
+  user 'deploy'
+  group 'apache'
   command 'php composer.phar require guzzlehttp/guzzle'
 end
